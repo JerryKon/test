8fabfca559491f23c13306840310e70443d224a6
Jerry <jerrychinese@163.com>
2022-03-26 15:20:49 +0800

    Update main.cpp
    add default value to vector and print the vector

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 int main()
 {
   cout<<"test for git";
-  std::vector<float> testvector;
+  std::vector<float> testvector{1,25,6,68,8};
+  for(auto v : testvector)
+    std::cout<<v<<" ";
   return 0;
   }
